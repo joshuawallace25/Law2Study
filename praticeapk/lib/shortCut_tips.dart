f2eed0db30a9484a91f2840a25319ddf77b58481
joshuawallace25 <131925695+joshuawallace25@users.noreply.github.com>
2024-09-18 11:09:15 -0700

    new commit

---
+++ praticeapk/lib/shortCut_tips.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomwPage extends StatefulWidget {
+  const HomwPage({super.key});
+
+  @override
+  State<HomwPage> createState() => _HomwPageState();
+}
+
+class _HomwPageState extends State<HomwPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+//to create a Stateful Widget the short cut is stf and vs code will display a promt to stateful component
+//to create A Stateless widget the short cut is stl and vs code wll displaay the prompt to stateless component
+//alt + shift + A it comment a hilighted block of code into a comment
+//alt shift f arrange your code into the rightful Structure
