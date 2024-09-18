b7e63f10f14c28efc37e64c59e5b249441487b34
TheLaw-20 <lawrence21maurice@gmail.com>
2024-09-18 17:37:00 +0200

    new commit

--- praticeapk/lib/main.dart
+++ praticeapk/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
      
-      home: const MyHomePage(),
+      home: const MyHomePage(g),
     );
   }
 }
