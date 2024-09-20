dad21a09d2dca6f3e08319a3df62e94e3d3a8489
joshuawallace25 <131925695+joshuawallace25@users.noreply.github.com>
2024-09-20 15:05:25 -0700

    hello

--- praticeapk/lib/view/home_view.dart
+++ praticeapk/lib/view/home_view.dart
@@ -6,104 +6,136 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My App",),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: ListView(
-         // padding: EdgeInsets.zero,
-          children:[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child:  Column(children: [
-                Text(
-                'John Brown',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-              Text(
-                'John Brown',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-              ],)
-            ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                // Handle home tap
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.account_circle),
-              title: const Text('Profile'),
-              onTap: () {
-                // Handle profile tap
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                // Handle settings tap
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: const Text('Logout'),
-              onTap: () {
-                // Handle logout tap
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text(
+            "My App",
+          ),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
         ),
-      ),
-      body: const Column(
-        children: [
-          Text(
-        "Our First Application",
-        style: TextStyle(
-          color: Colors.red,
-          // fontSize: ,
-           fontWeight: FontWeight.bold,
-           
-        ),),
-
-        SizedBox(height: 10,),
-           Text(
-        "Our First Application",
-        style: TextStyle(
-          color: Colors.red,
-          // fontSize: ,
-           fontWeight: FontWeight.bold,
-           
-        ),),
-           Text(
-        "Our First Application",
-        style: TextStyle(
-          color: Colors.red,
-          // fontSize: ,
-           fontWeight: FontWeight.bold,
-           
-        ),)
-        ],
-
-      )
-    );
+        drawer: Drawer(
+          child: ListView(
+            // padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        'John Brown',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                      Text(
+                        'John Brown',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  )),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {
+                  // Handle home tap
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.account_circle),
+                title: const Text('Profile'),
+                onTap: () {
+                  // Handle profile tap
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                onTap: () {
+                  // Handle settings tap
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Logout'),
+                onTap: () {
+                  // Handle logout tap
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            const Text(
+              "Our First Application",
+              style: TextStyle(
+                color: Colors.red,
+                // fontSize: ,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20,
+              right: 30),
+              child: const Text(
+                "Our First Application",
+                style: TextStyle(
+                  color: Colors.red,
+                  // fontSize: ,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Text(
+              "Our First Application",
+              style: TextStyle(
+                color: Colors.red,
+                // fontSize: ,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.star),
+                Text('Row Example'),
+                Icon(Icons.star_border),
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print("button is press");
+                },
+                child: const Text("Submit")),
+            GestureDetector(
+              onTap: (){
+                 print("Gesture detector button is press");
+              },
+              child: Container(
+                  color: Colors.amber,
+                  height: 50,
+                  width: 100,
+                  child: Center(child: Text('Tap Me')),
+                ),
+            ),
+            
+          ],
+        ));
   }
 }
-
-
