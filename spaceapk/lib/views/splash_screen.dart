4c12708d71bb34bb27a6982774337da429433734
TheLaw-20 <lawrence21maurice@gmail.com>
2024-09-22 03:25:50 +0400

    splash screen

---
+++ spaceapk/lib/views/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:spaceapk/views/home_view.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: 350,
+          color: Colors.blue,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 200.0),
+                child: Text('S P A C E  A P P',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold
+                ),),
+              ),
+              const SizedBox(height: 50.0,),
+              ElevatedButton(onPressed: (){
+                Navigator.push( context,
+                MaterialPageRoute(
+                builder: (context) => const HomePage()));
+              }, 
+              child: const Text('Next Page')),
+            ],
+          ),
+        ),
+      );
+  }
+}
