2d0e272fa8525ae42a2b58c8f13045e65ac3f7e1
TheLaw-20 <lawrence21maurice@gmail.com>
2024-09-21 07:03:54 +0400

    new update

--- spaceapk/lib/views/home_view.dart
+++ spaceapk/lib/views/home_view.dart
@@ -15,22 +15,51 @@ class HomePage extends StatelessWidget {
         color: Colors.black,
         child: Column(
           children: [
-            const Text(
-              'You are on Earth',
-              style: TextStyle(color: Colors.white),
+            const Padding(
+              padding: EdgeInsets.only(top: 50.0),
+              child: Text(
+                'You are on Earth',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-            Image.asset("assets/earth.jpeg"),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,MaterialPageRoute(
-                    builder: (context) => const SecondPage()));
-                },
-                child: const Text('Go to the moon')),
-            const Text('Click here to go to the moon'),
+            const SizedBox(
+              height: 50.0,
+            ),
+            Image.asset("assets/earth.jpeg", width: 300.0, height: 300.0),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Click here to go to the Moon',
+                    style: const TextStyle(color: Colors.white, fontSize: 12.0),
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SecondPage()));
+                      },
+                      child: const Text(
+                        'Go to the moon',
+                        style: TextStyle(fontSize: 10.0),
+                      )),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
-
   }
 }
--- spaceapk/lib/views/second_view.dart
+++ spaceapk/lib/views/second_view.dart
@@ -11,18 +11,58 @@ class SecondPage extends StatelessWidget {
         color: Colors.black,
         child: Column(
           children: [
-            const Text('You are on the Moon',
-            style: TextStyle(
-              color: Colors.white),),
-            Image.asset("assets/moon.jpeg"),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => const HomePage()));
-              }, 
-              child: const Text("travel Now")),
-              const Text('Click here to go to the Moon')
+            const Padding(
+              padding: EdgeInsets.only(top: 50.0),
+              child: Text(
+                'You are on the Moon',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(
+              height: 50.0,
+            ),
+            Image.asset(
+              "assets/moon.jpeg",
+              width: 300.0,
+              height: 300.0,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Click here to go to the Moon',
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomePage()));
+                      },
+                      child: const Text(
+                        "travel Now",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                        ),
+                      )),
+                ],
+              ),
+            ),
+            const Text('Click here to go to the Moon')
           ],
         ),
       ),
