993a6ec0a3adda5c1a028d76b79c220dfa414c89
TheLaw-20 <lawrence21maurice@gmail.com>
2024-09-30 14:54:51 +0400

    chat room

--- getxapk/lib/views/home_view.dart
+++ getxapk/lib/views/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; // Assuming you're using GetX for navigation
-import 'package:getxapk/chat/circle.dart'; // Import your custom widgets
-import 'package:getxapk/chat/rectangle.dart'; // Import your custom widgets
+import 'package:getxapk/chat/circle.dart';
+import 'package:getxapk/chat/rectangle.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -10,19 +9,70 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hello"),
-      ),
-      drawer: Drawer(),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Home Page',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-          ],
+        backgroundColor: Colors.grey[200],
+        leading: IconButton(
+          onPressed: (){},
+           icon: Icon(Icons.arrow_back_ios,
+           color: Colors.black,
+           size: 24,)),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 50.0),
+          child:  Text("Chat Room",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16
+          ),),
         ),
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Row(
+                        children: [
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                          SizedBox(width: 15,),
+                          Circle(),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 40,),
+                    Rectangle(),
+                    SizedBox(height: 6,),
+                    Rectangle(),
+                    SizedBox(height: 6,),
+                    Rectangle(),
+                    SizedBox(height: 6,),
+                    Rectangle(),
+                    SizedBox(height: 6,),
+                    Rectangle(),
+                    SizedBox(height: 6,),
+                    Rectangle(),
+                  ],
+                ),
+              ),
+            ),
+          ),
       ),
     );
   }
